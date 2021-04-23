d7ed613f551a1f57913f67f7ba4aa7d9831a060e
CAH-zachreyzimdars <zachrey.zimdars@cardinalhealth.com>
2021-04-23 12:50:04 -0500

    updating host contianer for root config and index

--- src/CAH-root-config.js
+++ src/CAH-root-config.js
@@ -1,36 +1,21 @@
 import { registerApplication, start } from "single-spa";
-import todo from "../../Todo-app/todo/src/index";
-// import {
-//   constructApplications,
-//   constructRoutes,
-//   constructLayoutEngine,
-// } from "single-spa-layout";
-// import microfrontendLayout from "./microfrontend-layout.html";
+// import todo from "../../Todo-app/todo/src/index";
+import {
+  constructApplications,
+  constructRoutes,
+  constructLayoutEngine,
+} from "single-spa-layout";
+import microfrontendLayout from "./microfrontend-layout.html";
 
-// const routes = constructRoutes(microfrontendLayout);
-// const applications = constructApplications({
-//   routes,
-//   loadApp({ name }) {
-//     return System.import(name);
-//   },
-// });
-// const layoutEngine = constructLayoutEngine({ routes, applications });
+const routes = constructRoutes(microfrontendLayout);
+const applications = constructApplications({
+  routes,
+  loadApp({ name }) {
+    return System.import(name);
+  },
+});
+const layoutEngine = constructLayoutEngine({ routes, applications });
 
-// layoutEngine.isActive();
-
-// applications.forEach(registerApplication);
-// layoutEngine.activate();
-
-registerApplication(
-    '@CAH/todo',
-    () => import('@CAH/todo'),
-    // () => import('../../Todo-app/todo/src/index.js'),
-    (location) => location.pathname.startsWith('/')
-);
-
-// registerApplication(
-//     '@CAH/navbar',
-//     () => import('../../Navbar-app/navbar/src/index.js'),
-//     (location) => location.pathname.startsWith('/')
-// );
+applications.forEach(registerApplication);
+layoutEngine.activate();
 start();
